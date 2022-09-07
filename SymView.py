5754058f5fb606282c0c364eb28dbdf1489e4811
Alex <temper8@gmail.com>
2022-09-07 17:11:54 +0300

    Update SymView.py

--- SymView.py
+++ SymView.py
@@ -138,14 +138,14 @@ class SymView:
 
 
 	def make_palette(self, clr_num):
-		N = clr_num * 200
+		N = clr_num * 100
 		pal = np.zeros(N, dtype=np.uint32)
 		for i in range(0, clr_num):
 			c1 = self.Colors[i]
 			c2 = self.Colors[(i+1)%clr_num]
-			for j in range(0, 200):
-				d = float(j)/200
-				pal[i*200+j] = avg_clr(c1,c2,d,225)
+			for j in range(0, 100):
+				d = float(j)/100
+				pal[i*100+j] = avg_clr(c1,c2,d,225)
 		return pal	
 
 	def Draw(self):
