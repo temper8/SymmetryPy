d4338d59154d6431b6ba5f3d8ed0e09f9203240d
Alex <temper8@gmail.com>
2023-03-26 02:02:41 +0300

    add examples

---
+++ DebugWindow.py
@@ -0,0 +1,12 @@
+# Copyright (c) RedFantom 2017
+# Copyright (c) Juliette Monsel 2017
+# For license see LICENSE
+
+from ttkwidgets import DebugWindow
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+ttk.Button(root, text="Print ok", command=lambda: print('ok')).pack()
+DebugWindow(root)
+root.mainloop()
